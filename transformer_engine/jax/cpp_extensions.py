[JAX] bugfix for softmax lowering (#218)
bugfix for softmax lowering

Signed-off-by: Ryan Jeng <rjeng@nvidia.com>

transformer_engine/jax/cpp_extensions.py
@@ -37,6 +37,8 @@ def te_dtype_to_jax_dtype(te_dtype):
         return jnp.bfloat16
     if te_dtype == TEDType.kInt32:
         return jnp.int32
+    if te_dtype == TEDType.kInt64:
+        return jnp.int64
     return jnp.int8
 
 
@@ -1675,7 +1677,7 @@ class ScaledSoftmaxBwdPrimitive(SoftmaxPrimitive):
                                                          grad_outputs, softmax_outputs,
                                                          scale_factor)
 
-        return [out]
+        return out # out is iterable already
 
 
 _scaled_softmax_bwd_p = register_primitive(ScaledSoftmaxBwdPrimitive)
@@ -1824,7 +1826,7 @@ class ScaledMaskedSoftmaxBwdPrimitive(SoftmaxPrimitive):
                                                          grad_outputs, softmax_outputs,
                                                          scale_factor)
 
-        return [out]
+        return out # out is iterable already
 
 
 _scaled_masked_softmax_bwd_p = register_primitive(ScaledMaskedSoftmaxBwdPrimitive)
@@ -1958,7 +1960,7 @@ class ScaledUpperTriangMaskedSoftmaxBwdPrimitive(SoftmaxPrimitive):
             ScaledUpperTriangMaskedSoftmaxBwdPrimitive.name, ctx, grad_outputs, softmax_outputs,
             scale_factor)
 
-        return [out]
+        return out # out is iterable already
 
 _scaled_upper_triang_masked_softmax_bwd_p = \
     register_primitive(ScaledUpperTriangMaskedSoftmaxBwdPrimitive)